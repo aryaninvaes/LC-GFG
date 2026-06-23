0f9bf8575cac7c8ae2e5afdc21ae54c4f054d017
aryaninvaes <aryaninvaes@gmail.com>
2026-06-23 19:52:15 +0530

    Time: 387 ms (2.11%), Space: 516.6 MB (5.23%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int i=0;
-        int j=0;
-
-        while(i<s.size() && j<t.size()){
-            if(s[i]==t[j]){
-                i++;
-            }
-            j++;
+    bool function(string s, string t, int index, int currIndex, vector<vector<int>>& dp){
+        if(dp[index][currIndex]!=-1){
+            return dp[index][currIndex];
         }
-        if(i==s.size()) return true;
-        else return false;
+
+        if(currIndex==s.size()){
+            return true;
+        }
+        if(index==t.size()){
+            return false;
+        }
+        bool take=false; 
+        if(s[currIndex]==t[index]){
+            take= function(s,t,index+1, currIndex+1,dp);
+        }
+        bool skip= function(s,t, index+1, currIndex,dp);
+        return dp[index][currIndex] = take || skip;
+    }
+    bool isSubsequence(string s, string t) {
+        int n= s.size();
+        int m=t.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return function(s,t,0,0,dp);
     }
 };
