30a6cd211918868fdbe51b0865c88ee6d455bb1a
aryaninvaes <aryaninvaes@gmail.com>
2026-06-23 19:55:20 +0530

    Update 0392-is-subsequence.cpp

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -25,3 +25,19 @@ public:
         return function(s,t,0,0,dp);
     }
 };
+// // two pointer approach
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int i = 0, j = 0;
+
+//         while(i < s.size() && j < t.size()) {
+//             if(s[i] == t[j]) {
+//                 i++;
+//             }
+//             j++;
+//         }
+
+//         return i == s.size();
+//     }
+// };
