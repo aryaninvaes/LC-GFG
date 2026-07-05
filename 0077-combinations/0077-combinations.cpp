477671f375edbbd6c07fd5adab83fa1e76777aa6
Utkarsh Shankar <aryaninvaes@gmail.com>
2026-07-05 13:33:56 +0530

    Time: 75 ms (33.23%), Space: 82.5 MB (35.26%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -7,19 +7,43 @@ public:
             result.push_back(temp);
             return;
         }
-        if(start>n){
-            return;
+        for(int i=start; i<=n; i++){
+            temp.push_back(i);
+            solve(i+1, n, k-1, temp);
+            temp.pop_back();
         }
-
-        temp.push_back(start);
-        solve(start+1, n, k-1, temp);
-        temp.pop_back();
-        solve(start+1, n,k,temp );
-        }
+    }
 
     vector<vector<int>> combine(int n, int k) {
         vector<int> temp;
-        solve(1, n ,k, temp);
+        solve(1, n, k, temp);
         return result;
+        
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> result;
+
+//     void solve(int start, int n, int k, vector<int>& temp){
+//         if(k==0){
+//             result.push_back(temp);
+//             return;
+//         }
+//         if(start>n){
+//             return;
+//         }
+
+//         temp.push_back(start);
+//         solve(start+1, n, k-1, temp);
+//         temp.pop_back();
+//         solve(start+1, n,k,temp );
+//         }
+
+//     vector<vector<int>> combine(int n, int k) {
+//         vector<int> temp;
+//         solve(1, n ,k, temp);
+//         return result;
+//     }
+// };
