3e92f5c4a3fec9f4bce253d87d8a68190ba3ab29
Utkarsh Shankar <aryaninvaes@gmail.com>
2026-07-13 02:29:21 +0530

    Time: 0 ms (100%), Space: 10.9 MB (13.19%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,19 +2,41 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0) return false;
+        string temp=to_string(x);
+        int left=0;
+        int right=temp.length()-1;
 
-        int n=x;
-        vector<int> digits;
-
-        while(n){
-            digits.push_back(n%10);
-            n /= 10;
+        while(left<right){
+            if(temp[left]!=temp[right]){
+                return false;
+            }
+            left++;
+            right--;
         }
-        long long pro=0;
-        for(auto d: digits){
-            pro=pro*10+d;
-        }
-        return pro==x;
+        return true;
         
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if(x<0) return false;
+
+//         int n=x;
+//         vector<int> digits;
+
+//         while(n){
+//             digits.push_back(n%10);
+//             n /= 10;
+//         }
+//         long long pro=0;
+//         for(auto d: digits){
+//             pro=pro*10+d;
+//         }
+//         return pro==x;
+        
+//     }
+// };
