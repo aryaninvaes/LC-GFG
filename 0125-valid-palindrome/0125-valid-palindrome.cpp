ef353b1f7c2a3d41038873168c442219847cc796
Utkarsh Shankar <aryaninvaes@gmail.com>
2026-07-13 22:51:01 +0530

    Time: 0 ms (100%), Space: 10.4 MB (15.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,53 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string temp="";
-        for(char str: s){
-            if(isalnum(str)){
-                temp += tolower(str);
+        string temp = "";
+
+        for (char ch : s) {
+            if ((ch >= 'a' && ch <= 'z') ||
+                (ch >= 'A' && ch <= 'Z') ||
+                (ch >= '0' && ch <= '9')) {
+
+                if (ch >= 'A' && ch <= 'Z')
+                    ch = ch - 'A' + 'a';
+
+                temp += ch;
             }
         }
-        int left=0; 
-        int right=temp.length()-1;
-        while(left<right){
-            if(temp[left]!=temp[right]){
+
+        int left = 0;
+        int right = temp.size() - 1;
+
+        while (left < right) {
+            if (temp[left] != temp[right])
                 return false;
-            }
+
             left++;
             right--;
         }
+
         return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         string temp="";
+//         for(char str: s){
+//             if(isalnum(str)){
+//                 temp += tolower(str);
+//             }
+//         }
+//         int left=0; 
+//         int right=temp.length()-1;
+//         while(left<right){
+//             if(temp[left]!=temp[right]){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+// };
